0b05c45f37a7cb72fafb64e1ff2bd27635abbe71
sanskar360 <sanskarvijaybhilavade@gmail.com>
2025-12-01 18:32:23 +0530

    completed UI of Home Page

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template,request
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/add_orders")
+def add_orders():
+    return render_template("add_orders.html")
+
+@app.route("/track_orders")
+def track_orders():
+    return render_template("track_orders.html")
+
+@app.route("/payments")
+def payments():
+    return render_template("payments.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
